5e648a7c61591b4ed02d348f69434030ee98459f
Big Brother <root@192.168.43.131>
2015-02-12 07:18:48 +0000

    global env variables set

--- read_profile.cc
+++ read_profile.cc
@@ -1,64 +1,144 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<syslog.h>
 
-char** readFile(const char *filename, size_t *lineCount){
+#define HOME_ENV_NAME "HOME"
+#define PATH_ENV_NAME "PATH"
+#define MAX_LINE_LEN 1024
+#define DEBUG 0
+
+char *gHOME = NULL;
+char *gPATH = NULL;
+
+void set_path_value(char* str)
+{
+	gPATH = strdup(str);
+}
+
+void set_name_value(char* str)
+{
+	gPATH = strdup(str);
+}
+
+
+int begins_with(const char* line, char *name, int *pos)
+{
+	const char *temp_line = line;
+	int i=0;
+	while((temp_line != NULL) && *temp_line == ' ')
+	{
+#if DEBUG
+    printf("Entered firstWhile\n");
+#endif
+		temp_line++;
+		i++;
+	}
+	int j=0;
+        while(temp_line[j] == name[j] && temp_line[j] != NULL && name[j] != NULL)
+	{
+#if DEBUG
+    printf("Entered secondWhile\n");
+#endif
+		j++;
+	}
+#if DEBUG
+    printf("i= %d\n", i);
+#endif
+	if(name[j] == NULL)
+	{
+#if DEBUG
+    printf("i= %d\n", i);
+#endif
+		*pos = i + j - strlen(name);
+		return 1;
+	}
+	*pos = 0;
+	return 0;
+}
+
+int processEnvVars(char* line)
+{
+   char value[MAX_LINE_LEN];
+   int pos;
+   if(line[0] == '#')
+   {
+	printf("Ignoring comment line\n");
+	return 0;
+   }
+   printf("%s\n", line);
+   if(begins_with(line, "export", &pos))
+   {
+	int i;
+	pos = pos + strlen("export");
+	while(line != NULL && line[pos] == ' ')
+		pos++;
+	for(i = 0; line[i + pos] != NULL; i++)
+		line[i] = line[i+pos]; 	
+	line[i] = NULL;
+   }
+
+   pos=0;
+   if(begins_with(line, PATH_ENV_NAME, &pos))
+   {
+	int i;
+	pos = pos + strlen(PATH_ENV_NAME);
+	if(line[pos] == '=')
+	{
+		pos++;
+		for(i = 0; line[i + pos] != NULL; i++)
+			value[i] = line[i+pos]; 	
+		value[i] = NULL;
+   		printf("Name=%s Value=%s",PATH_ENV_NAME, value);
+		set_path_value(value);
+	}
+   }
+
+   pos=0;
+   if(begins_with(line, HOME_ENV_NAME, &pos))
+   {
+	int i;
+	pos = pos + strlen(HOME_ENV_NAME);
+	if(line[pos] == '=')
+	{
+		pos++;
+		for(i = 0; line[i + pos] != NULL; i++)
+			value[i] = line[i+pos]; 	
+		value[i] = NULL;
+   		printf("Name=%s Value=%s",HOME_ENV_NAME, value);
+		set_name_value(value);
+	}
+   }
+
+	
+}
+
+int readFile(const char *filename, size_t *lineCount){
 	FILE *fp;
 	char buff[4096];
-	size_t lines = 0, capacity=1024;
-	char **line;
 
 	if(NULL==(fp=fopen(filename, "r"))){
     		perror("file can't open.");
-		return NULL;
+		return -1;
 	}
-	if(NULL==(line=(char**)malloc(sizeof(char*)*capacity))){
-		perror("can't memory allocate.");
-		fclose(fp);
-	    	return NULL;
-	}
+	int i = 0;
 	while(NULL!=fgets(buff, sizeof(buff), fp)){
-	    line[lines++] = strdup(buff);
-	    if(lines == capacity){
-		    capacity += 32;
-		    if(NULL==(line=(char**)realloc(line, sizeof(char*)*capacity))){
-			perror("can't memory allocate.");
-		        fclose(fp);
-			return NULL;
-		    }
-	    }
-       }
-    *lineCount = lines;
-    fclose(fp);
-    return (char**)realloc(line, sizeof(char*)*lines);
-}
-
-void freeMem(char** p, size_t size){
-	size_t i;
-
-	if(p==NULL) return;
-
-	for(i=0;i<size;++i)
-		free(p[i]);
-    	free(p);
+	    printf("Line %d:", i++);
+	    processEnvVars(buff);
+       	}
+    	fclose(fp);
 }
 
 int main()
 {
-	char* file_name = "/root/.profile";
+	char* file_name = "/root/.myprofile";
 	size_t lines; 
 	char **line;
-
-	if(NULL==(line=readFile(file_name, &lines)))
+	if(-1 == readFile(file_name, &lines))
 	{
 		printf("Error reading file: %s\n", file_name);
 		return 1;
 	}
 
-	for(int i=0; i < lines; i++)
-	{
-		printf("%s\n", line[i]);
-	}
-	freeMem(line, lines);
 	return 0;
 }
